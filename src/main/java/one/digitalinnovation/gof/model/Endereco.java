e7acf528f9b1134b7ae23e4ef7b191844811a331
peterson-007 <84036142+peterson-007@users.noreply.github.com>
2022-06-11 14:06:15 -0300

    Update Endereco.java

--- src/main/java/one/digitalinnovation/gof/model/Endereco.java
+++ src/main/java/one/digitalinnovation/gof/model/Endereco.java
@@ -5,7 +5,7 @@ import javax.persistence.Id;
 
 /**
  * Os atributos desse modelo foram gerados automaticamente pelo site
- * jsonschema2pojo.org. Para isso, usamos o JSON de retorno da API do ViaCEP.
+ * jsonschema2pojo.org. Para isso, foi utilizado o JSON de retorno da API do ViaCEP.
  * 
  * @see <a href="https://www.jsonschema2pojo.org">jsonschema2pojo.org</a>
  * @see <a href="https://viacep.com.br">ViaCEP</a>
